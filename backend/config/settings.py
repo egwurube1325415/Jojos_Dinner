3b2ca3b1d488896feecfe05a318941e86422ba66
Petah <poegwurube@gmail.com>
2021-09-01 22:04:19 -0500

    added backend api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.posts',
+    'apps.items',
+    'apps.reviews',
     'cloudinary',
     'django_filters',
 ]
